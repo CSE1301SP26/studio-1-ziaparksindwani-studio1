f4f3e802cca6e88ec5b108323b1761cec4b32e4a
ssindwani <s.sienna@wustl.edu>
2026-01-21 15:29:21 -0600

    hello

---
+++ src/studio1/leapYear.java
@@ -0,0 +1,14 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class leapYear{
+public static void main(String[] args){
+
+		Scanner in = new Scanner(System.in);
+		System.out.print("Year? ");
+		int year = in.nextInt();
+        boolean leapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+        System.out.println(year + " is a leap year: " + leapYear);
+    }
+}
